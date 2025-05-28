f628da1bac5bf9611f863268ad0f19371a4b66f2
MicheleCasani <casanimichele@gmail.com>
2025-05-28 15:44:11 +0200

    [UPD] add show

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -4,27 +4,54 @@ const mysql = require('../data/db.js');
 const connection = require('../data/db.js');
 
 
-
-
 // index
 const index = (req, res) => {
 
-    // salvare in una variabile la query da utilizzare
+    // salvo in una variabile la query da utilizzare
     const sql = 'SELECT * FROM posts';
 
-    // eseguire la query per mostrare i post
+    // eseguo la query per mostrare i post
     connection.query(sql, (err, results) => {
         if (err) return res.status(500).json({ error: 'Database error' });
         res.json(results);
     })
 };
 
+
 // show
 const show = (req, res) => {
-    res.send(`post con id: ${req.params.id}`)
+    // recupero id
+    const id = parseInt(req.params.id);
+
+    // salvo in una variabile la query da utilizzare
+    const postSql = 'SELECT * FROM posts WHERE id = ?';
+
+    const tagsSql = `
+    SELECT *
+    FROM tags
+    JOIN post_tag ON post_tag.tag_id = tags.id
+    WHERE post_id = ?
+    `;
+
+    // eseguo la query per mostrare il singolo post
+    connection.query(postSql, [id], (err, postResults) => {
+        if (err) return res.status(500).json({ error: 'Database error' });
+        if (postResults.length === 0) return res.status(404).json({ error: 'Post non trovato' });
+
+        // recupero il post
+        const post = postResults[0];
+
+        // eseguo la  query per i tags
+        connection.query(tagsSql, [id], (err, tagsResults) => {
+            if (err) return res.status(500).json({ error: 'Database error' });
+
+            res.json(post);
+        })
+    })
 };
+
 
 module.exports = {
     index,
-    show
+    show,
 }
--- routers/post_router.js
+++ routers/post_router.js
@@ -14,16 +14,7 @@ router.get('/', postsController.index);
 //show 
 router.get('/:id', postsController.show);
 
-// // store 
-// router.post('/', postsController.store);
-
-// // update 
-// router.put('/:id', postsController.update);
-
-// // modify 
-// router.patch('/:id', postsController.modify);
-
-// // destroy 
+// //  destroy 
 // router.delete('/:id', postsController.destroy);
 
 // esportazione router
