c9093a133e51182878f1cc640734c30ff20eb741
MicheleCasani <casanimichele@gmail.com>
2025-05-28 15:31:49 +0200

    [UPD]

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,6 +1,22 @@
+// importo mysql 
+const mysql = require('../data/db.js');
+// importo connection
+const connection = require('../data/db.js');
+
+
+
+
 // index
 const index = (req, res) => {
-    res.send("elenco post")
+
+    // salvare in una variabile la query da utilizzare
+    const sql = 'SELECT * FROM posts';
+
+    // eseguire la query per mostrare i post
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database error' });
+        res.json(results);
+    })
 };
 
 // show
---
+++ data/db.js
@@ -0,0 +1,21 @@
+// importo mysql2
+
+// importare mysql
+const mysql = require('mysql2');
+
+// configurazioni MySql 
+const connection = mysql.createConnection({
+
+    host: 'localhost',
+    user: 'root',
+    password: 'xxxxxxx',
+    database: 'db_blog'
+})
+
+connection.connect((err) => {
+
+    if (err) throw err;
+    console.log('Connesso a MySql');
+})
+
+module.exports = connection;
