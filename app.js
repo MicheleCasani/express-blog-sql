d89d9a7ef33f5f74d496dab2180b084f6061d5a3
MicheleCasani <casanimichele@gmail.com>
2025-05-28 14:44:03 +0200

    [UPD]

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("ciao");
+// importo express
+const express = require('express');
+const app = express()
+const port = 3000;
+
+
+// rotta base
+app.get("/", (req, res) => {
+    console.log("server del mio blog")
+    res.send("benvenuto nella mia pizzeria")
+})
+
+app.listen(port, () => {
+    console.log("server in ascolto sulla porta" + port)
+})
